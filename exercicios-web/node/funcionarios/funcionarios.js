b59ba2192ef6a50f950286b60d7990587b1d6384
vittorioaugusto <vi.aborges47@gmail.com>
2024-07-31 11:59:33 -0300

    adicionando json

--- exercicios-web/node/funcionarios/funcionarios.js
+++ exercicios-web/node/funcionarios/funcionarios.js
@@ -1,15 +1,21 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
-const chineses = f => f.pais === 'China'
-const mulheres = f => f.genero === 'F'
-const menorSalario = (func, funcAtual) => {
-    return func.salario < funcAtual.salario ? func : funcAtual
+function chineses(f) {
+    return f.pais === 'China';
+}
+
+function mulheres(f) {
+    return f.genero === 'F';
+}
+
+function menorSalario(func, funcAtual) {
+    return func.salario < funcAtual.salario ? func : funcAtual;
 }
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    // console.log(funcionarios)
+    console.log(funcionarios)
 
     // mulher chinesa com menor salário?
     const func = funcionarios
